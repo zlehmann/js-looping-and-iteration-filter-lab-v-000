235ac81264ad485d9c1da4abe9ccf38503e5a5f5
Zachary Lehmann <zlehmann@gmail.com>
2019-03-14 23:32:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function findMatching(drivers, str) {
   const newDrivers = drivers.filter(driver => driver.toLowerCase() === str.toLowerCase());
   return newDrivers;
 }
+
+function fuzzyMatch (drivers, str) {
+  let len = str.length();
+  const newDrivers = drivers.filter(driver => driver.substr(0,len) == str);
+  return newDrivers;
+}
