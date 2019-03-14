32076a7e4a0f4052716b7ddf994a819722c12474
Zachary Lehmann <zlehmann@gmail.com>
2019-03-14 23:29:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Code your solution in this file
+function findMatching(drivers, str) {
+  const newDrivers = drivers.filter(driver => driver.toLowerCase() === str.toLowerCase());
+  return newDrivers;
+}
